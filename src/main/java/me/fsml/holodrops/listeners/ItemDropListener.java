fde087411a27da8a4f538062b74ecbc109f96fdd
fsml <drewpy99@gmail.com>
2019-10-22 22:05:59 -0400

    cache, potential NPE fix, better lore blacklist

--- src/main/java/me/fsml/holodrops/listeners/ItemDropListener.java
+++ src/main/java/me/fsml/holodrops/listeners/ItemDropListener.java
@@ -20,19 +20,26 @@ public class ItemDropListener implements Listener {
         Item drop = e.getEntity();
         if (Main.m.settings.isWorldEnabled(drop.getWorld().getName())) {
             ItemStack item = drop.getItemStack();
-            if (checkBlacklistLore(item.getItemMeta())) {
-                return;
+            if (item.hasItemMeta()) {
+                if (checkBlacklistLore(item.getItemMeta())) {
+                    return;
+                }
+                if (Strings.hasWatermark(item)) {
+                    Strings.removeWatermark(item);
+                }
             }
-            Strings.removeWatermark(item);
-            String name = Strings.makeName(drop, item.getAmount());
-            drop.setCustomName(name);
+            if (!Main.m.settings.getProtectedDrops().containsKey(drop)) {
+                String name = Strings.makeName(drop, item.getAmount(), "", 0);
+                drop.setCustomName(name);
+            }
             drop.setCustomNameVisible(true);
             
             if (Main.m.settings.getItemGlow()) {
-                if (Main.m.settings.isGlowlisted(Strings.makeItemName(drop))) { // check the raw name
+                String rawName = Strings.makeItemName(drop);
+                if (Main.m.settings.isGlowlisted(rawName)) { // check the raw name
                     drop.setGlowing(true);
                     if (Main.m.settings.getGlowColor()) {
-                        ChatColor color = Glow.getColor(Strings.makeItemName(drop));
+                        ChatColor color = Glow.getColor(rawName);
                         Glow.setGlowColor(color, drop);
                     }
                 }
@@ -48,7 +55,7 @@ public class ItemDropListener implements Listener {
         
         if (meta.hasLore()) {
             for (String s : meta.getLore()) {
-                if (s.contains("Display")) {
+                if (s.contains("Display Item")) {
                     return true;
                 }
             }
