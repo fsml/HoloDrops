a667457a9ec2c7232afab3daf17c0fca380b3817
fsml <drewpy99@gmail.com>
2019-10-22 22:08:17 -0400

    more settings, protected drops tracker

--- src/main/java/me/fsml/holodrops/util/Settings.java
+++ src/main/java/me/fsml/holodrops/util/Settings.java
@@ -1,6 +1,8 @@
 package me.fsml.holodrops.util;
 
 import me.fsml.holodrops.Main;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +15,9 @@ public class Settings {
     private List<String> enabledWorlds;
     private List<String> blacklist;
     private List<String> glowlist;
+    private List<String> protitemlist;
+    private HashMap<Item, Player> protectedDrops = new HashMap<Item, Player>();
+    private int protTime;
     
     public void initialize() {
         Main.m.reloadConfig();
@@ -22,17 +27,22 @@ public class Settings {
         enabledWorlds = ConfigReader.getStringList("enabled-worlds");
         blacklist = ConfigReader.getStringList("blacklist");
         glowlist = Strings.colorList(ConfigReader.getStringList("glowlist"));
+        protitemlist = ConfigReader.getStringList("protection-item-list");
         
         settings.put("item-frame-holos", ConfigReader.getBoolean("item-frame-holos"));
         settings.put("custom-names-only", ConfigReader.getBoolean("custom-names-only"));
         settings.put("single-stack", ConfigReader.getBoolean("single-stack"));
         settings.put("item-glow", ConfigReader.getBoolean("item-glow"));
         settings.put("glow-color", ConfigReader.getBoolean("glow-color"));
+        settings.put("drop-protection", ConfigReader.getBoolean("drop-protection"));
+        settings.put("protect-block-drops", ConfigReader.getBoolean("protection-sources.block-drops"));
+        protTime = ConfigReader.getInt("protection-time");
         
         format.put("stack-count", Strings.color(ConfigReader.getString("stack-count")));
         format.put("holo-prefix", Strings.color(ConfigReader.getString("holo-prefix")));
         format.put("holo-suffix", Strings.color(ConfigReader.getString("holo-suffix")));
         format.put("holo-format", Strings.color(ConfigReader.getString("holo-format")));
+        format.put("protection-format", Strings.color(ConfigReader.getString("protection-format")));
         
         for (String configMaterial : Main.m.getConfig().getConfigurationSection("item-names").getKeys(false)) {
             String mat = Main.m.getConfig().getString("item-names." + configMaterial);
@@ -76,14 +86,19 @@ public class Settings {
         return format.get("holo-format");
     }
     
+    public String getProtFormat() {
+        return format.get("protection-format");
+    }
+    
     public boolean isWorldEnabled(String world) {
         return enabledWorlds.contains(world);
     }
     
     public boolean isBlacklisted(String name) {
         for (String s : blacklist) {
-            if (Strings.stripColor(s).equals(Strings.stripColor(name)))
+            if (Strings.stripColor(s).equals(Strings.stripColor(name))) {
                 return true;
+            }
         }
         return false;
     }
@@ -105,4 +120,36 @@ public class Settings {
         }
     }
     
+    public boolean protectionEnabled() {
+        return settings.get("drop-protection");
+    }
+    
+    public String protectionFormat() {
+        return format.get("protection-format");
+    }
+    
+    public List<String> getProtItemList() {
+        return protitemlist;
+    }
+    
+    public boolean getBlockProtection() {
+        return settings.get("protect-block-drops");
+    }
+    
+    public int getProtTime() {
+        return protTime;
+    }
+    
+    public HashMap<Item, Player> getProtectedDrops() {
+        return protectedDrops;
+    }
+    
+    public void fixNames() {
+        for (Item item : protectedDrops.keySet()) {
+            if (item != null) {
+                item.setCustomName(Strings.makeName(item, item.getItemStack().getAmount(), "", 0));
+            }
+        }
+        protectedDrops.clear();
+    }
 }
