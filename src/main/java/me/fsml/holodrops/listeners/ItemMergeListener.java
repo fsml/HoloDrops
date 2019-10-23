b8a97da0eed316882746c14bbb750d79be7dc54b
fsml <drewpy99@gmail.com>
2019-10-22 22:06:21 -0400

    merge and drop protection

--- src/main/java/me/fsml/holodrops/listeners/ItemMergeListener.java
+++ src/main/java/me/fsml/holodrops/listeners/ItemMergeListener.java
@@ -13,12 +13,28 @@ public class ItemMergeListener implements Listener {
     public void itemMerge(ItemMergeEvent e) {
         Item drop = e.getTarget();
         Item ent = e.getEntity();
+        // if theyre both protected compare the owners
+        if (isItemProtected(drop) && isItemProtected(ent)) {
+            if (Main.m.settings.getProtectedDrops().get(drop) != Main.m.settings.getProtectedDrops().get(ent)) {
+                e.setCancelled(true);
+                return;
+            }
+        }
+        // if only 1 is protected they dont merge
+        else if (isItemProtected(drop) || isItemProtected(ent)) {
+            e.setCancelled(true);
+            return;
+        }
         int count = ent.getItemStack().getAmount() + drop.getItemStack().getAmount();
         if (Main.m.settings.isWorldEnabled(drop.getWorld().getName())) {
-            String name = Strings.makeName(drop, count);
+            String name = Strings.makeName(drop, count, "", 0);
             drop.setCustomName(name);
             drop.setCustomNameVisible(true);
         }
+    }
+    
+    private boolean isItemProtected(Item item) {
+        return Main.m.settings.getProtectedDrops().containsKey(item);
     }
     
 }
