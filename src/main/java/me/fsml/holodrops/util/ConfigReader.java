9fd5f4fc296057242d0d075d0dcbff8ae82fb654
fsml <drewpy99@gmail.com>
2019-12-14 14:22:37 -0500

    add helper method for check command

--- src/main/java/me/fsml/holodrops/util/ConfigReader.java
+++ src/main/java/me/fsml/holodrops/util/ConfigReader.java
@@ -1,6 +1,9 @@
 package me.fsml.holodrops.util;
 
 import me.fsml.holodrops.Main;
+import org.bukkit.Material;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigReader {
@@ -19,6 +22,21 @@ public class ConfigReader {
     
     public static int getInt(String option) {
         return Main.m.getConfig().getInt(option);
+    }
+    
+    public static List<Material> getMissingItems() {
+        ArrayList<Material> configMats = new ArrayList<Material>();
+        for (String configMaterial : Main.m.getConfig().getConfigurationSection("item-names").getKeys(false)) {
+            configMats.add(Material.getMaterial(configMaterial));
+        }
+        for (Material m : Material.values()) {
+            if (!configMats.contains(m)) {
+                configMats.add(m);
+            } else {
+                configMats.remove(m);
+            }
+        }
+        return configMats;
     }
     
 }
